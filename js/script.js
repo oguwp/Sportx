429d5a40252cbc6f4a0485c59ca32ff9d1530433
KennethGoodspeed <60501913+KennethGoodspeed@users.noreply.github.com>
2020-05-25 16:36:51 +0200

    Om os sektion og fetch api

--- js/script.js
+++ js/script.js
@@ -19,6 +19,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+// GET API PRICE 1
+const getPrice1BtnElement = document.getElementById("getPrice1");
+
+getPrice1BtnElement.addEventListener("click", getPrice1);
+
+function getPrice1() {
+    fetch('./api/price1.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '<h2>extra priser</h2>';
+            data.forEach(function (data) {
+                output += `
+              <ul>
+                <li>${data.text1}</li>
+                <li>${data.text2}</li>
+                <li>${data.text3}</li>
+                <li>${data.text4}</li>
+              </ul>
+            `;
+            })
+            document.getElementById('output').innerHTML = output;
+        })
+}
+
 // GET API ABOUT US
 const getAboutBtnElement = document.getElementById("getAbout");
 
